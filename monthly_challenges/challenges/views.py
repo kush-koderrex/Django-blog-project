cd7e957815c0ceddc1c270ab22d2ddfdccc95780
kush-koderrex <kushkumar89792666@gmail.com>
2024-04-02 08:44:36 +0530

    add challenges

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
 
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 # Create your views here.
 month_chal={
-    "jan":"Jan",
-    "feb":"feb",
+    "jan":"Jan is my birtyh day",
+    "feb":"feb is Frbulious",
     "mar":"mar",
     "april":"april",
     "may":"may",
@@ -16,18 +17,30 @@ month_chal={
     "sep":"sep",
     "oct":"oct",
     "nov":"nov",
-    "dec":"dec",
+    "dec":None,
 }
 
-def index(request,month):
-    return HttpResponse()
+def index(request):
+    list_itms = ""
+    months = list(month_chal.keys())
+    return render(request, "challenges/index.html",{
+        "months" : months
+    })
+ 
+    # for month in months:
+    #     cap_month = month.capitalize()
+    #     month_path =reverse("month-challenge",args=[month])
+    #     list_itms += f"<li><a href = '{month_path}'>{cap_month}</a></li>"
+
+    # response_data = f"<ol>{list_itms}</ol>"
+    # return HttpResponse(response_data)
     
     
 
 def monthly_challenge_by_number(request,month):  
     months = list(month_chal.keys())
     if month > len(months):
-        return HttpResponseNotFound("Endpoint Not Found ")
+        return HttpResponseNotFound("Endpoint Not Found")
         
     redirect_url = months[month-1]
     redirect_path = reverse("month-challenge",args=[redirect_url])
@@ -39,7 +52,12 @@ def monthly_challenge_by_number(request,month):
 def monthly_challenge(request,month):
     try:
         chal_text = month_chal[month]
-        response_data = f"<h1>{chal_text}<h1/>"
-        return HttpResponse(response_data)
+        cap_month = month.capitalize()
+        return render(request,"challenges/challenge.html",{
+            "text":chal_text,
+            "month_name":cap_month,
+        })
+        # response_data = render_to_string("challenges/challenge.html")
+        # return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("Endpoint Not Found ")
--- monthly_challenges/monthly_challenges/urls.py
+++ monthly_challenges/monthly_challenges/urls.py
@@ -19,5 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', admin.site.urls),
+    path('',  include("challenges.urls")),
     path('challenges/', include("challenges.urls")),
 ]
